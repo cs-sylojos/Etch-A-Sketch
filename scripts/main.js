13701d6aadf56f5e6a3843182f5cbcc6acb52bc4
syuong <112560745+cs-sylojos@users.noreply.github.com>
2023-06-13 23:29:51 +0800

    Add button to allow user to change canvas size

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,79 @@
 const sketchPad = document.querySelector('.sketchPad');
-let size = 16 * 16;
-for (let i = 0; i < size; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.classList.add('default');
-    grid.setAttribute('style', `height: 37.5px; width: 37.5px; border: 1px solid grey;`);
-    sketchPad.appendChild(grid);
+const sizeSelection = document.querySelector('.size');
+
+let sizeInput = "16";
+let canvasSize = Number(sizeInput);
+let previousSize;
+let isSketchPadUpdate = true;
+
+updateSketchPad();
+hoverEffect();
+
+function resetSketchPad() {
+    if (isSketchPadUpdate) {
+        let numberOfGrid = previousSize * previousSize;
+        const gridToRemove = document.querySelectorAll('.grid');
+        for (let j = 0; j < numberOfGrid; j++) {
+            gridToRemove.forEach((elementGrid) => {
+                elementGrid.remove();
+            });
+        }
+    }
 }
 
-const gridList = document.querySelectorAll('.grid');
-gridList.forEach((squareGrid) => {
-    squareGrid.addEventListener('mouseover', (e) => {
-        e.target.classList.toggle('hoverColor');
+function updateSketchPad() {
+    if (isSketchPadUpdate) {
+        let size = canvasSize * canvasSize;
+        let gridDimension = 600 / canvasSize;
+        for (let i = 0; i < size; i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.classList.add('default');
+            grid.setAttribute('style', `height: ${gridDimension}px; width: ${gridDimension}px; border: 1px solid grey;`);
+            sketchPad.appendChild(grid);
+        }
+    }
+}
+
+function hoverEffect() {
+    const gridList = document.querySelectorAll('.grid');
+    gridList.forEach((squareGrid) => {
+        squareGrid.addEventListener('mouseover', (e) => {
+            e.target.classList.toggle('hoverColor');
+        });
+        squareGrid.addEventListener('mouseout', (e) => {
+            e.target.classList.toggle('hoverColor');
+        });
     });
-    squareGrid.addEventListener('mouseout', (e) => {
-        e.target.classList.toggle('hoverColor');
-    });
+}
+
+sizeSelection.addEventListener('click', () => {
+    sizeInput = prompt("Please enter a number from 1 to 100", "16");
+    if (sizeInput === null || sizeInput === "" || sizeInput.trim() === "") {
+        sizeInput = canvasSize.toString();
+        isSketchPadUpdate = false;
+    } else if (Number(sizeInput.trim()) > 100) {
+        alert("Size too big!");
+        sizeInput = canvasSize.toString();
+        isSketchPadUpdate = false;
+    } else if (isNaN(Number(sizeInput.trim()))) {
+        alert("Enter a number!");
+        sizeInput = canvasSize.toString();
+        isSketchPadUpdate = false;
+    } else if (!Number.isInteger(Number(sizeInput.trim()))) {
+        alert("Enter a whole number!");
+        sizeInput = canvasSize.toString();
+        isSketchPadUpdate = false;
+    } else if (Number(sizeInput.trim()) >= 1 && Number(sizeInput.trim()) <= 100) {
+        sizeInput = sizeInput.trim();
+        isSketchPadUpdate = true;
+    }
+    previousSize = canvasSize;
+    canvasSize = Number(sizeInput);
+    resetSketchPad();
+    updateSketchPad();
+    if (isSketchPadUpdate) {
+        hoverEffect();
+    }
+    isSketchPadUpdate = false;
 });
